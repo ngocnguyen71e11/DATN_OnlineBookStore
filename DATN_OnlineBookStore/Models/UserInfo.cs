d28005b364feff4781f27825908698bc12211637
Mins_HN <Mins_HN@DESKTOP-HOBK3H5>
2024-08-09 23:44:35 +0700

    commit theloai

---
+++ DATN_OnlineBookStore/Models/UserInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace DATN_OnlineBookStore.Models
+{
+    public class UserInfo
+    {
+        public int FkITaikhoanId { get; set; }
+        public int PkSKhid { get; set; }
+        public string SHo { get; set; }
+        public string STen { get; set; }
+        public string SSdt { get; set; }
+        public DateTime? DNgaysinh { get; set; }
+        public string SNghenghiep { get; set; }
+        public string Diachi { get; set; }
+        public string Thanhpho { get; set; }
+        public string QuanHuyen { get; set; }
+        public string XaPhuong { get; set; }
+    }
+
+}
